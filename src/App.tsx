2ed7285bc4e3c767d9d48e015e0faa0a0c626e63
selmanpak <selmanpak@gmail.com>
2026-06-03 20:29:04 +0300

    Use home hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { HomeHeroSection } from './sections/home/HomeHeroSection';
 
 export default function App() {
   return (
     <div className="app-shell">
       <Header />
-      <main className="home-placeholder">
-        <div className="home-placeholder__inner">
-          <h1>Hello World</h1>
-        </div>
+      <main>
+        <HomeHeroSection />
       </main>
       <Footer />
     </div>
