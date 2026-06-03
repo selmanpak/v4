16aae7ed1265c3cce8f0e0881038af20b9d17611
selmanpak <selmanpak@gmail.com>
2026-06-03 21:27:30 +0300

    Add section 2 to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { HomeHeroSection } from './sections/home/HomeHeroSection';
+import { HomeSection2 } from './sections/home/HomeSection2';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <main>
         <HomeHeroSection />
+        <HomeSection2 />
       </main>
       <Footer />
     </div>
