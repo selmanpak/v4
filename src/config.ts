08f2d30eeedad037143ff6d4091d1888c61f4f87
selmanpak <selmanpak@gmail.com>
2026-06-03 20:19:32 +0300

    Add FirstData header and footer

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const navItems = [
+  { label: 'Platform', path: '#platform' },
+  { label: 'Solutions', path: '#solutions' },
+  { label: 'Use Cases', path: '#use-cases' },
+  { label: 'About', path: '#about' },
+  { label: 'Contact', path: '#contact' },
+];
