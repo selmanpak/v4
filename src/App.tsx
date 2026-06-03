805157d94a3bb7f8832360a358f8bc78101cafd7
selmanpak <selmanpak@gmail.com>
2026-06-03 20:21:59 +0300

    Add FirstData header and footer to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="app-shell">
+      <Header />
+      <main className="home-placeholder">
+        <div className="home-placeholder__inner">
+          <h1>Hello World</h1>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 }
